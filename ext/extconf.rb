7db2b2b6dd0989c0e8fb7d362310b8dc547ea9a2
Adam Ingram-Goble <adamaig@gmail.com>
2009-03-10 14:11:26 -0400

    more cleanup

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,8 @@
 require 'mkmf'
-$CFLAGS << "-x c++ -I."
-$LDFLAGS << " -L. -Wl,-rpath,. -law_sdk.41 -lstdc++"
+
 dir_config("awsdk")
+
+$CFLAGS << "-x c++"
+$LDFLAGS << "-Wl,-rpath,. -law_sdk.41 -lstdc++"
+
 create_makefile("ruby_activeworld")
